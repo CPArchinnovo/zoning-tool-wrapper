526e3f5b383e78fa26b16bb2fa0b4e844f1f3453
Chris Polaski <cp@archinnovo.com>
2025-06-29 15:23:25 -0400

    fix: point to private zoning-core and clean reqs

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-import os, sys, subprocess
-import streamlit as st
+import os, sys, subprocess, streamlit as st
 
-token = os.environ["GITHUB_TOKEN"]                    # comes from Streamlit secrets
-repo_url = f"git+https://__token__:{token}@github.com/CParchinnovo/zoning-core.git@main"
-
-# one-time install / upgrade
+token = os.environ["GITHUB_TOKEN"]
+repo_url = f"git+https://{token}:x-oauth-basic@github.com/CParchinnovo/zoning-core.git@main"
 subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", repo_url])
 
-from zoning_core import run_app                       # now it imports fine
+from zoning_core import run_app
 
 if __name__ == "__main__":
     run_app()
-
